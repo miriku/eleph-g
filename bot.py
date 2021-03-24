89066065224ffc26f6315029891b63e2575d24d6
miriku <me@miriku.com>
2021-03-24 13:12:59 -0700

    now splitting of content online and no longer a single post

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import os
+import game
+import discord
 from urllib.request import urlopen
 from discord.ext import tasks
 from discord.ext import commands
@@ -7,33 +9,43 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 OUTPUTCHANNEL = os.getenv('OUTPUT_CHANNEL')
+WEBSITESTRING = os.getenv('WEBSITE_STRING')
+URLOFLIST = os.getenv('URL_OF_DATA_FILE')
+DEBUG = os.getenv('DEBUG_LEVEL')
 
 bot = commands.Bot(command_prefix='!')
 
-# bookmark. TODO: create a concept of a state for each game, including owner, when it's closing, and players in it.
+# bot's state
+games = {}
 
+# helper command: link to website with message
 @bot.command(name='new', help='Generates a personal link to let the bot know you\'re looking for people to play with.')
 async def new(ctx):
-    await ctx.send("{}, please click this link to register a new game: http://miriku.com/newGame?user={}".format(ctx.message.author.mention, ctx.message.author.id))
+  await ctx.send("{}, please click this link to register a new game: {}{}".format(ctx.message.author.mention, WEBSITESTRING, ctx.message.author.id))
 
+# main loop 
 @tasks.loop(seconds=10)
-async def mainLoop(post):
-    f = urlopen("https://miriku.com/list.txt")
-    ls = f.readlines()
-    output = ""
+async def mainLoop():
+  f = urlopen(URLOFLIST)
+  ls = f.readlines()
 
-    # bookmark. TODO: make this be a tokenizer that parses actual content and is statefully aware of whether a game is 
-    #   just opening, waiting for players, has enough players now, or is expired
+  for l in ls:
+    line_data = l.split(b"\t")     
+    # is this a new game?
+    if not line_data[0] in games:
+      if DEBUG:
+        print("initializing game" + str(line_data[0]))
+      # add it, first create a game object
+      g = game.Game(line_data[0])
+      games[line_data[0]]=g
 
-    for l in ls:
-      output = output + l.decode('utf-8') + "\n"
-    await post.edit(content=output)
-
-# bookmark. TODO: add respone to reaction
-
+# performed once when the bot starts up
 @bot.event
 async def on_ready():
-    post = await OUTPUTCHANNEL.send("Initializing")
-    mainLoop.start(post)
+  if DEBUG:
+    print("starting\n")
+  channel = discord.utils.get(bot.get_all_channels(), guild__name='IRC Boardgames', name='bots')
+  await channel.send("Initializing")
 
+mainLoop.start()
 bot.run(TOKEN)
