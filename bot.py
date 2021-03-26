35f7234e4619bf12eb255e3f19f5abdc9d7a0833
miriku <me@miriku.com>
2021-03-25 19:55:29 -0700

    reactions are now able to pull game objects back from the game array

--- bot.py
+++ bot.py
@@ -25,6 +25,30 @@ games = {}
 async def new(ctx):
   await ctx.send("{}, please click this link to register a new game: {}{}".format(ctx.message.author.mention, WEBSITESTRING, ctx.message.author.id))
 
+# respond to reaction
+#  - first let the website know that a new person joined
+#  - then if it's full, message everyone
+@bot.event
+async def on_reaction_add(reaction, user):
+  emoji = reaction.emoji
+  message = reaction.message
+  thisgame = ""
+
+  if user.bot:
+    return
+
+  if emoji == "✅":
+    thisGame=-1
+    for game in games.values():
+      if game.message.id == message.id: 
+        thisgame = game
+        
+    if DEBUG:
+      print("Reaction to game:  ")
+      print(game.text)
+  else:
+    return
+
 # main loop 
 @tasks.loop(seconds=10)
 async def mainLoop():
@@ -40,9 +64,13 @@ async def mainLoop():
       if DEBUG:
         print("initializing game", line_data)
       # add it, first create a game object
-      g = game.Game(line_data[0])
+      message = await channel.send(line_data[1])
+      emoji = '✅'
+      await message.add_reaction(emoji)
+      g = game.Game(line_data[0], message, line_data[1])
       games[line_data[0]]=g
-      await channel.send(line_data[1])
+
+  # BOOKMARK TODO remove entries that are expired
 
 # performed once when the bot starts up
 @bot.event
@@ -50,7 +78,7 @@ async def on_ready():
   if DEBUG:
     print("starting\n")
   channel = discord.utils.get(bot.get_all_channels(), guild__name=OUTPUTSERVER, name=OUTPUTCHANNEL)
- # await channel.send("Initializing")
+  await channel.send("Initializing")
   mainLoop.start()
 
 bot.run(TOKEN)
--- game.py
+++ game.py
@@ -1,14 +1,17 @@
 class Game:
   id = -1
-  name = ""
+  text = ""
+  message = -1 
   owner = -1
   maxPlayers = -1
   currentPlayers = []
   timeClose = -1
   post = 0
 
-  def __init__(self, id):
+  def __init__(self, id, message, text):
     self.id = id
+    self.message = message
+    self.text = text
 
   def debug():
     print("game")
